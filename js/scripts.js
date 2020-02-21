8cebc2c25b9d6e99f6abbd68b1affca2df1f8f7f
Mohammad Desai <mdesai1096@gmail.com>
2020-02-21 16:39:23 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,42 @@
 "use-strict";
+// use-strict catches code errors before they cause damage
 
-const theDate = document.getElementById("date");
-const datePicker = document.querySelector('input[type="date"]');
+/*
+  the containing IIFE (Immediately-invoked Function Expression)
+  keeps your variables out of the global namespace
+*/
 
-const template = theDate.innerText;
+(function () {
 
-function showDate() {
-  const d = new Date(datePicker.value);
-  buildDate(d)
-}
+  // DATE =======================================
+  const showDate = () => {
+    let d = new Date(datePicker.value);
+    theDate.innerText = d.toDateString();
+  }
+  datePicker.addEventListener("change", showDate);
+  // shows today's date on load:
+  let today = new Date();
+  theDate.innerHTML = today.toDateString();
 
-function buildDate(d){
-template = `${template} ${d.toDateString()}`
-}
 
-datePicker.addEventListner('Change', showDate)
+  // COLOUR =====================================
+  const showColour = () => {
+    let c = theColour.value;
+    colourBox.style.backgroundColor = c;
+  }
+  theColour.addEventListener("change", showColour);
+  // shows the colour on load:
+  showColour();
+
+
+  // RANGE ======================================
+  const showRange = () => {
+    let r = theRange.value;
+  	// note: the '%' unit is essential
+  	let rangeUnit = `${r}%`;
+    colourBox.style.width = rangeUnit;
+  	colourBox.innerText = rangeUnit;
+  }
+  theRange.addEventListener("input", showRange);
+
+})();
